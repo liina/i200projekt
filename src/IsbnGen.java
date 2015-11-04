2d06f72e71e9dcc5f2dee4f36464cbabf23e61f5
Liina Abner <liina.abner@gmail.com>
2015-11-05 00:09:07 +0200

    algeline fx

--- src/IsbnGen.java
+++ src/IsbnGen.java
@@ -1,3 +1,12 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,25 +14,66 @@ import java.lang.Math;
 
 /**
  * Created by Liina Abner on 30.09.2015.
- Programm palub sisestada kirjastustunnuse õigest vahemikust.
- Genereerib vastuseks kõik sinna vahemiku kuuluvad ISBN numbrid.
- ISBN number on 13-kohaline: prefix-riigitunnus-rühmatunnus-raamatutunnus-kontrollnumber.
- Programm peab kontrollima, kas kasutaja sisestatud number kuulub Eestile eraldatud rühmatunnuste hulka.
- Programm peab genereerima kõik isbn numbrid, mis antud rühmatunnuse sees võimalikud on st. leidma raamatutunnused ja kontrollnumbri.
- Teeks GUI. Osa siseandmeid loeks xml failist. Väljundi kirjutaks faili.
- Kui mingi rühmatunnus on juba võetud, siis selle paneks lukku. Päriselus ab, aga praegu lihtsalt csv-s.
+ Programm palub sisestada kirjastustunnuse Ãµigest vahemikust.
+ Genereerib vastuseks kÃµik sinna vahemiku kuuluvad ISBN numbrid.
+ ISBN number on 13-kohaline: prefix-riigitunnus-rÃ¼hmatunnus-raamatutunnus-kontrollnumber.
+ Programm peab kontrollima, kas kasutaja sisestatud number kuulub Eestile eraldatud rÃ¼hmatunnuste hulka.
+ Programm peab genereerima kÃµik isbn numbrid, mis antud rÃ¼hmatunnuse sees vÃµimalikud on st. leidma raamatutunnused ja kontrollnumbri.
+ Teeks GUI. Osa siseandmeid loeks xml failist. VÃ¤ljundi kirjutaks faili.
+ Kui mingi rÃ¼hmatunnus on juba vÃµetud, siis selle paneks lukku. PÃ¤riselus ab, aga praegu lihtsalt csv-s.
  Lisaks suvalise isbn numbri valiidsuse kontroll.
  Triipkoodi genereerimine?
  Lk. 27 Kontrollnumbri arvutamise algoritm https://www.isbn-international.org/sites/default/files/Manual_Estonian_2013.pdf
  ISBN vahemikud saab siit https://www.isbn-international.org/export_rangemessage.xml
  978-9985 - prefix ja Eesti riigitunnus
- Võimalikud kirjastustunnused:
+ VÃµimalikud kirjastustunnused:
  0-4
  50-79
  800-899
  9000-9999
  */
-public class IsbnGen {
+public class IsbnGen extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        int prefix = 978;
+        int riik = 9985;
+        final int[] ktunnus = new int[1];
+        VBox vbox = new VBox();
+        Scene scene = new Scene(vbox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        Label seletus = new Label("Vali number vahemikest\n 0-4\n" +
+                " 50-79\n" +
+                " 800-899\n" +
+                " 9000-9999");
+        TextField input = new TextField();
+        Button nupp = new Button();
+        nupp.setText("genereeri rmtu tunnused");
+        Label numbrid = new Label();
+        nupp.setOnAction(event -> {
+            ktunnus[0] = Integer.parseInt(input.getText()); //lambda expression what
+            String[] rtunnus = new String[0];
+            try {
+                rtunnus = genRmt(ktunnus[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            for (int i=0;i<rtunnus.length;i++) {
+                //System.out.print(rtunnus[i]);
+                String number = "" + prefix + riik + ktunnus + rtunnus[i]; //kui ei pane "" ette, siis teeb mingi huvitava liitmise
+                int ktrlnumber = 0;
+                try {
+                    ktrlnumber = genKtrl(number);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                number = prefix + "-" + riik + "-" + ktunnus[0] + "-" + rtunnus[i] + "-" + ktrlnumber;
+                numbrid.setText(numbrid.getText() + "\n" + number);
+            }
+        });
+        vbox.getChildren().addAll(seletus,input,nupp,numbrid);
+    }
+    /*
     public static void main(String[] args) throws IOException {
         int prefix = 978;
         int riik = 9985;
@@ -32,7 +82,7 @@ public class IsbnGen {
         System.out.println("Sisesta kirjastustunnus vahemikest:"); // siia vahemike spikker
         sisend = reader.readLine();
         int ktunnus = Integer.parseInt(sisend); //siia kontroll, kas sisend mahub etteantud vahemikesse
-        System.out.println("Sain kätte" + ktunnus);
+        System.out.println("Sain kï¿½tte" + ktunnus);
         String[] rtunnus = genRmt(ktunnus);
         for (int i=0;i<rtunnus.length;i++) {
             //System.out.print(rtunnus[i]);
@@ -41,30 +91,31 @@ public class IsbnGen {
             System.out.format("%d-%d-%d-%s-%d%n",prefix,riik,ktunnus,rtunnus[i],ktrlnumber);
         }
     }
+    */
     public static String[] genRmt (int ktunnus) throws IOException {
         //genereeri numbrid
         String[] rtunnus;
-        //tuleb leida mitmekohaline number ktunnus on, sellest sõltub mitmekohaline saab olla raamatutunnus
+        //tuleb leida mitmekohaline number ktunnus on, sellest sï¿½ltub mitmekohaline saab olla raamatutunnus
         //variant 1 - parsida ktunnus stringiks ja leida stringi pikkus
         int kpikkus = (Integer.toString(ktunnus)).length();
-        //isbn kokku 13 numbrit, prefix+riigitunnus võtab 7 kohta, kontrollnumber võtab 1 koha, kirjastustunnuse ja raamatutunnse peale kokku jääb 5 kohta
-        int rpikkus = 5 - kpikkus; //raamatutunnuse pikkuseks jääb 5-kirjastustnnus
+        //isbn kokku 13 numbrit, prefix+riigitunnus vï¿½tab 7 kohta, kontrollnumber vï¿½tab 1 koha, kirjastustunnuse ja raamatutunnse peale kokku jï¿½ï¿½b 5 kohta
+        int rpikkus = 5 - kpikkus; //raamatutunnuse pikkuseks jï¿½ï¿½b 5-kirjastustnnus
         System.out.println("rpikkus " + rpikkus);
-        int count; //mitu raamatutunnust on st. kui raamatutunnuse pikkus on x, siis saab üldse olla 10 astmel x numbrit. Näiteks kui raamatutunnuse pikkus on 3, siis on võimalikke tunnuseid 1000
+        int count; //mitu raamatutunnust on st. kui raamatutunnuse pikkus on x, siis saab ï¿½ldse olla 10 astmel x numbrit. Nï¿½iteks kui raamatutunnuse pikkus on 3, siis on vï¿½imalikke tunnuseid 1000
         count = (int)Math.pow((double) 10, (double) rpikkus);
         rtunnus = new String[count];
         for (int i=0; i<count; i++) {
-            int lisanull; //rtunnuse ette on vaja lisada nulle, et raamatutunnuses oleks õige arv märke
+            int lisanull; //rtunnuse ette on vaja lisada nulle, et raamatutunnuses oleks ï¿½ige arv mï¿½rke
             lisanull = rpikkus-Integer.toString(i).length();
             String rmt = Integer.toString(i);
-            if (lisanull > 0) { //kui i-s on vähem kohti kui rmttunnuse jaoks ettenähtud, siis
+            if (lisanull > 0) { //kui i-s on vï¿½hem kohti kui rmttunnuse jaoks ettenï¿½htud, siis
                 String nll = "0";
                 lisanull--;
                 while (lisanull > 0){
                     nll = nll + "0";
                     lisanull--;
                 }
-                rmt = nll+ rmt; //lisab ette niimitu 0, kuipalju on vaja, et raamatutunnuses oleks õige arv märke
+                rmt = nll+ rmt; //lisab ette niimitu 0, kuipalju on vaja, et raamatutunnuses oleks ï¿½ige arv mï¿½rke
             }
 
             rtunnus[i] = rmt;
@@ -78,9 +129,9 @@ public class IsbnGen {
         //ktrl numbri algoritm
         //korrutab iga paarispositsioonil oleva numbri 1-ga
         //ja paaritul positsioonil 3-ga. Liidab kokku,
-        //leiab jäägi summa jagamisel 10-ga,
-        //lahutab saadud jäägi 10-st,
-        //saadud arvu 10-ga jagamisel saadud jääk ongi kontrollnumber
+        //leiab jï¿½ï¿½gi summa jagamisel 10-ga,
+        //lahutab saadud jï¿½ï¿½gi 10-st,
+        //saadud arvu 10-ga jagamisel saadud jï¿½ï¿½k ongi kontrollnumber
         for (int i = 0;i < number.length(); i++){
             int t = Character.getNumericValue(number.charAt(i));
             if (i%2 == 0) {
@@ -93,6 +144,7 @@ public class IsbnGen {
         ktrlnr = (10-(summa % 10)) % 10;
         return ktrlnr;
     }
+
 
 
 }
